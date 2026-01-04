96357d623c38df1246d75957489eda58289ca724
Leison44 <Lps44de@gmail.com>
2026-01-04 20:56:12 -0300

    Implement conditional structure for age verification

--- Aula05.java
+++ Aula05.java
@@ -1,1 +1,17 @@
-bdhfgfkinufgfgf
+public class Aula05 {
+            //Estruturas Condicionais//
+        //Aula 20//
+    public static void main(String[]args){
+        int age= 17;
+        boolean isayulautorizadocomprarbebida= age >= 18;
+        if(isayulautorizadocomprarbebida){
+            System.out.println("autorizado a comprar bebida");
+        }
+        //Aula 21//
+        else{
+            System.out.println("Não pode");
+        }
+            //Esse print so aperecera enquanto if for verdade//
+    }
+ }
+    
